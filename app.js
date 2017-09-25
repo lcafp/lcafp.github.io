e292a2234a27604ef2565d08f4577ac286024b5d
Jules Bonnard <julesbonnard@gmail.com>
2017-09-25 16:57:32 +0200

    babel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.onload = function(e) { 
+window.onload = function(e) {
   var router = new VueRouter({
       mode: 'history',
       routes: []
@@ -6,32 +6,47 @@ window.onload = function(e) {
 
   var app = new Vue({
     router,
+    
     el: '#app',
+
     components: {
       'credits': httpVueLoader('./vendor/afp/credits.vue'),
       'logo': httpVueLoader('./vendor/afp/logo.vue'),
       'tabs': httpVueLoader('./tabs.vue')
     },
+
     data: {
-      story: {}
+      story: {},
+      loaded: false
     },
-    created() {
-    	const url = this.getStory(this.$route.query.lang);
-      this.loadStory(url).then(json => {
-        this.story = json;
-      })
+
+    created () {
+      var url = this.getStory(this.$route.query.lang);
+      this.loadStory(url);
     },
-    mounted() {
-    	var pymChild = new pym.Child({ polling: 500 });
+
+    mounted () {
+      this.pymChild = new pym.Child({ polling: 500 });
     },
+
     methods: {
-    	getStory(lang) {
-        if(lang && lang.length == 2) return './story_'+lang+'.txt';
+      getStory (lang) {
+        if(lang && lang.length === 2) return './story_' + lang + '.txt';
         return './story_en.txt';
       },
-      loadStory(url) {
+
+      loadStory (url) {
         return this.$http.get(url)
-          .then(response => archieml.load(response.bodyText));
+          .then(response => archieml.load(response.bodyText))
+          .then(
+            json => {
+              this.story = json;
+              this.loaded = true;
+            },
+            error => {
+              this.loadStory('./story_en.txt');
+            }
+          );
       }
     }
   })
